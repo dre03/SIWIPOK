798d8b83a7a5f9b3f7eb30a4c1c4679474e22710
dre03 <andreapriyana23@gmail.com>
2022-07-08 11:30:15 +0700

    constuct again

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,6 +2,7 @@
 class Home extends CI_Controller{
     function __construct()
     {
+        parent::__construct();
         $this->load->model('m_wisata','wisata');
     }
     public function index(){

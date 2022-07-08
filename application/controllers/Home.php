f9b7e83c8b1a7240645547d317028dc90ead446e
dre03 <andreapriyana23@gmail.com>
2022-07-08 11:36:29 +0700

    fix autoload

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,32 +1,32 @@
 <?php
 class Home extends CI_Controller{
-    function __construct()
-    {
-        // parent::__construct();
-        $this->load->model('M_wisata','wisata');
-    }
+
     public function index(){
-        echo "HAIII";
-		// $data['jenis'] = $this->wisata->get_jenis();
-		// $data['kecamatan'] = $this->wisata->get_kec();
-		// $data['wisata'] = $this->wisata->get_all();
-        // $this->load->view('Home/index', $data);
+        
+        $this->load->model('m_wisata');
+		$data['jenis'] = $this->m_wisata->get_jenis();
+		$data['kecamatan'] = $this->m_wisata->get_kec();
+		$data['wisata'] = $this->m_wisata->get_all();
+        $this->load->view('Home/index', $data);
     }
 
     public function home(){
-		$data['jenis'] = $this->wisata->get_jenis();
-		$data['kecamatan'] = $this->wisata->get_kec();
-		$data['wisata'] = $this->wisata->get_all();
+        $this->load->model('m_wisata');
+		$data['jenis'] = $this->m_wisata->get_jenis();
+		$data['kecamatan'] = $this->m_wisata->get_kec();
+		$data['wisata'] = $this->m_wisata->get_all();
         $this->load->view('Home/header');
         $this->load->view('Home/home', $data);
     }
     
     public function detail(){
+		$this->load->model('m_wisata');
         $id = $this->input->get('id');
-		$detail = $this->wisata->detail_data($id);
+		$detail = $this->m_wisata->detail_data($id);
 		$data['detail'] = $detail;
         $this->load->model('M_komentar');
 		$data['komentar'] = $this->M_komentar->getByWisataId($id);
 		$this->load->view('Home/detail', $data);
 	}
 }
+?>

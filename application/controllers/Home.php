52955cdc115f22360580453eb41d43d4cd89754a
dre03 <andreapriyana23@gmail.com>
2022-07-08 12:51:28 +0700

    hmm

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,26 +1,26 @@
 <?php
 class Home extends CI_Controller{
     public function index(){
-        $this->load->model('M_wisata');
-		$data['jenis'] = $this->M_wisata->get_jenis();
-		$data['kecamatan'] = $this->M_wisata->get_kec();
-		$data['wisata'] = $this->M_wisata->get_all();
+        $this->load->model('M_wisata','wisata');
+		$data['jenis'] = $this->wisata->get_jenis();
+		$data['kecamatan'] = $this->wisata->get_kec();
+		$data['wisata'] = $this->wisata->get_all();
         $this->load->view('Home/index', $data);
     }
 
     public function homes(){
-        $this->load->model('M_wisata');
-		$data['jenis'] = $this->M_wisata->get_jenis();
-		$data['kecamatan'] = $this->M_wisata->get_kec();
-		$data['wisata'] = $this->M_wisata->get_all();
+        $this->load->model('M_wisata','wisata');
+		$data['jenis'] = $this->wisata->get_jenis();
+		$data['kecamatan'] = $this->wisata->get_kec();
+		$data['wisata'] = $this->wisata->get_all();
         $this->load->view('Home/header');
         $this->load->view('Home/home', $data);
     }
     
     public function detail(){
-		$this->load->model('M_wisata');
+		$this->load->model('M_wisata','wisata');
         $id = $this->input->get('id');
-		$detail = $this->M_wisata->detail_data($id);
+		$detail = $this->wisata->detail_data($id);
 		$data['detail'] = $detail;
         $this->load->model('M_komentar');
 		$data['komentar'] = $this->M_komentar->getByWisataId($id);
